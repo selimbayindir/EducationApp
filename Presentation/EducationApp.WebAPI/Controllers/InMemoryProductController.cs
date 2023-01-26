cab56c5898541b3308747e69fa5b2581a5d84907
byndr <byndr28@gmail.com>
2023-01-26 20:18:27 +0300

    Controller settings

---
+++ Presentation/EducationApp.WebAPI/Controllers/InMemoryProductController.cs
@@ -0,0 +1,33 @@
+﻿using EducationApp.Domain.Entities;
+using EducationAPP.Application.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EducationApp.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InMemoryProductController : ControllerBase
+    {
+        readonly private IProductWriteRepository _productWriteRepository; 
+        readonly private IProductReadRepository _productReadRepository;
+
+        public InMemoryProductController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository)
+        {
+            _productWriteRepository = productWriteRepository;
+            _productReadRepository = productReadRepository;
+        }
+        [HttpGet("InMemorySaveProduct")]      
+        public async void Get() 
+        {
+            _productWriteRepository.AddRangeAsync(new()
+            {
+                new(){Id=Guid.NewGuid(),ProductName="Product 1",Price=100,CreatedDate=DateTime.UtcNow,Stock=10 },
+                new(){Id=Guid.NewGuid(),ProductName="Product 2",Price=200,CreatedDate=DateTime.UtcNow,Stock=20 },
+                new(){Id=Guid.NewGuid(),ProductName="Product 2",Price=300,CreatedDate=DateTime.UtcNow,Stock=130 }
+                });
+            await _productWriteRepository.SaveAsync();
+        }
+
+    }
+}
--- Presentation/EducationApp.WebAPI/Controllers/ProductController.cs
+++ Presentation/EducationApp.WebAPI/Controllers/ProductController.cs
@@ -8,7 +8,7 @@ namespace EducationApp.WebAPI.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
-        readonly private IProductWriteRepository _productWriteRepository; 
+        readonly private IProductWriteRepository _productWriteRepository;
         readonly private IProductReadRepository _productReadRepository;
 
         public ProductController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository)
@@ -16,16 +16,6 @@ namespace EducationApp.WebAPI.Controllers
             _productWriteRepository = productWriteRepository;
             _productReadRepository = productReadRepository;
         }
-        [HttpGet("inmemorySave")]      
-        public async void Get() 
-        {
-            _productWriteRepository.AddRangeAsync(new()
-            {
-                new(){Id=Guid.NewGuid(),ProductName="Product 1",Price=100,CreatedDate=DateTime.UtcNow,Stock=10 },
-                new(){Id=Guid.NewGuid(),ProductName="Product 2",Price=200,CreatedDate=DateTime.UtcNow,Stock=20 },
-                new(){Id=Guid.NewGuid(),ProductName="Product 2",Price=300,CreatedDate=DateTime.UtcNow,Stock=130 }
-                });
-            await _productWriteRepository.SaveAsync();
-        }
+       //HttpGet
     }
 }
